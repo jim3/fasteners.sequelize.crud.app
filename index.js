da3889233364017b0b4661b68ec1cc317f052897
jim3 <j3.casey@protonmail.com>
2023-04-02 16:14:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,28 @@ const app = express();
 
 // -=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=--=-=-=-=-=- //
 
-app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files from the "public" directory
+app.use("/public", express.static("public", {
+        setHeaders: (res, path, stat) => {
+            if (path.endsWith(".css")) {
+                res.setHeader("Content-Type", "text/css");
+            }
+
+            if (path.endsWith(".js")) {
+                res.setHeader("Content-Type", "text/javascript");
+            }
+
+            if (path.endsWith(".png")) {
+                res.setHeader("Content-Type", "image/png");
+            }
+        },
+    })
+);
+
+// Route for the home page
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
@@ -21,7 +39,9 @@ db.sequelize.sync().then(() => {
     console.log("Synced db.");
 });
 
-const port = 4000;
+const port = 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
+
+// -=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=--=-=-=-=-=- //

56f7ccebbc79474d3d753a41f42fd8464460fca5
jim3 <j3.casey@protonmail.com>
2023-04-02 14:37:46 -0400

    Update fastenerModel.js

--- models/fastenerModel.js
+++ models/fastenerModel.js
@@ -1,53 +1,41 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-// -=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=--=-=-=-=-=- //
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-// Set up sequelize connection to the database
 const sequelize = new Sequelize({
     dialect: "sqlite",
     storage: "parts.db",
+    logging: (...msg) => console.log(msg),
+    define: {
+        freezeTableName: true,
+    },
 });
 
-// Test the connection
 const testConnection = async () => {
     try {
         await sequelize.authenticate();
-        console.log("Connection has been established successfully.");
+        console.log("Connection has been established in PartNameModel successfully");
     } catch (error) {
         console.error("Unable to connect to the database:", error);
     }
 };
 testConnection();
 
-// -=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=--=-=-=-=-=- //
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-// Define the model
-const Fasteners = sequelize.define("Fastener", {
-    // Model attributes
-    partnumber: {
+// Model
+const PartName = sequelize.define("PartName", {
+    partname: {
         type: DataTypes.STRING,
-    },
-    description: {
-        type: DataTypes.STRING,
-    },
-    quantity: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    price: {
-        type: DataTypes.INTEGER,
         allowNull: false,
     },
 });
 
-// Sync the model with the database
-const syncModel = async () => {
-    await Fasteners.sync({ force: true });
-    console.log("The table for the Fastener model was just (re)created!");
-};
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-syncModel();
+// Export the model and the sequelize connection
+const db = { PartName, sequelize, Sequelize };
 
-// -=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=--=-=-=-=-=- //
+module.exports = db;
 
-module.exports = Fasteners;
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
